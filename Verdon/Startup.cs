0e13aba295bd5291c1be2a03f734eff0996a1c0a
Solomon yusuf <solomonyusufolayinka@gmail.com>
2023-01-02 07:02:48 +0100

    configure file permission

--- Verdon/Startup.cs
+++ Verdon/Startup.cs
@@ -2,16 +2,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 using Verdon.Areas.Identity;
 using Verdon.Data;
@@ -28,7 +32,7 @@ namespace Verdon
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+       
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<ApplicationDbContext>(options =>
@@ -58,9 +62,19 @@ namespace Verdon
                 app.UseHsts();
             }
 
+            //[SecurityPermission(SecurityAction.Demand, ControlThread = true)]
+
+            // configure static file permission
+            FileIOPermission permission = new FileIOPermission(FileIOPermissionAccess.Write, Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "StaticFiles"));
+            permission.Demand();
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot", "StaticFiles")),
+                RequestPath = new PathString("/wwwroot/StaticFiles")
+            });
             app.UseRouting();
 
             app.UseAuthentication();
