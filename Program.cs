2de26c9be42790dea3b8e59b08f7b2352ccf17f2
Solomon yusuf <solomonyusufolayinka@gmail.com>
2023-01-18 20:29:39 +0100

    try

--- Program.cs
+++ Program.cs
@@ -22,14 +22,14 @@ namespace Verdon
                 {
                     webBuilder.UseStartup<Startup>();
                     //// For running in Railway
-                    var portVar = Environment.GetEnvironmentVariable("PORT");
-                    if (portVar is { Length: > 0 } && int.TryParse(portVar, out int port))
-                    {
-                        webBuilder.ConfigureKestrel(options =>
-                        {
-                            options.ListenAnyIP(port);
-                        });
-                    }
+                    //var portVar = Environment.GetEnvironmentVariable("PORT");
+                    //if (portVar is { Length: > 0 } && int.TryParse(portVar, out int port))
+                    //{
+                    //    webBuilder.ConfigureKestrel(options =>
+                    //    {
+                    //        options.ListenAnyIP(port);
+                    //    });
+                    //}
                 });
     }
 }
